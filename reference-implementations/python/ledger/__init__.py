5e19204862a5bc7b6a692779701e2415fc653ef0
eternal-roman <github.2grnf@passmail.com>
2026-06-28 22:18:14 -0500

    chore(release): prepare v0.16.1

--- reference-implementations/python/ledger/__init__.py
+++ reference-implementations/python/ledger/__init__.py
@@ -53,4 +53,4 @@ __all__ = [
     "build_depreciation_schedule", "depreciation_to_entries",
 ]
 
-__version__ = "0.16.0-ref"  # align conceptually with TS
+__version__ = "0.16.1-ref"  # align conceptually with TS
